5827eef71facd09fb361abfa6f4d3a93abfaae47
Maulana Adil <maulanaadil.latif@gmail.com>
2022-05-07 15:28:24 +0700

    adding stylesheet into app

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,16 @@
+import type { LinksFunction } from '@remix-run/node';
+
+import stylerUrl from '~/styles/index.css';
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: stylerUrl,
+    },
+  ];
+};
+
 export default function Index() {
   return <div>Hello Wold Index Routes</div>;
 }

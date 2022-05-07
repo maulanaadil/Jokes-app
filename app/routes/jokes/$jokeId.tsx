14e7973b93184296a7d1a141e2c7d071ea60a323
Maulana Adil <maulanaadil.latif@gmail.com>
2022-05-07 18:01:50 +0700

    @feat/connect route  into data id joke in database

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,30 @@
+import { useLoaderData, Link } from '@remix-run/react';
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import type { Joke } from '@prisma/client';
+
+import { db } from '~/utils/db.server';
+
+type LoaderData = {
+  joke: Joke;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) throw new Error('Joke Not Found');
+  const data: LoaderData = { joke };
+  return json(data);
+};
+
 export default function JokeRoute() {
+  const { joke } = useLoaderData();
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{joke.content}</p>
+      <Link to='.'>{joke.name}</Link>
     </div>
   );
 }

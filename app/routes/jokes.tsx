98eb3d63d7fe58c23af3be9395457e802f4710d6
Maulana Adil <maulanaadil.latif@gmail.com>
2022-05-14 21:51:51 +0700

    User Authentication: - addding method getUser - implement method getUser for checking if user authenticated - adding logout method - implement logout method

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -4,6 +4,7 @@ import { Outlet, Link, useLoaderData } from '@remix-run/react';
 
 import stylesUrl from '~/styles/jokes.css';
 import { db } from '~/utils/db.server';
+import { getUser } from '~/utils/sessions.server';
 
 type Jokes = {
   id: string;
@@ -12,7 +13,8 @@ type Jokes = {
 };
 
 type LoaderData = {
-  jokoListItems: Array<Pick<Jokes, 'id' | 'name'>>;
+  user: Awaited<ReturnType<typeof getUser>>;
+  jokeListItems: Array<Pick<Jokes, 'id' | 'name'>>;
 };
 
 export const links: LinksFunction = () => {
@@ -24,19 +26,23 @@ export const links: LinksFunction = () => {
   ];
 };
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    select: { id: true, name: true },
+    orderBy: { createdAt: 'desc' },
+  });
+  const user = await getUser(request);
+
   const data: LoaderData = {
-    jokoListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: 'desc' },
-    }),
+    jokeListItems,
+    user,
   };
   return json(data);
 };
 
 export default function JoukesRoute() {
-  const { jokoListItems } = useLoaderData<LoaderData>();
+  const data = useLoaderData<LoaderData>();
   return (
     <div className='jokes-layout'>
       <header className='jokes-header'>
@@ -47,6 +53,18 @@ export default function JoukesRoute() {
               <span className='logo-medium'>😋</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className='user-info'>
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action='/logout' method='post'>
+                <button type='submit' className='button'>
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to='/login'>Login</Link>
+          )}
         </div>
       </header>
 
@@ -56,7 +74,7 @@ export default function JoukesRoute() {
             <Link to='.'>Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              {jokoListItems.map((joke) => (
+              {data.jokeListItems.map((joke) => (
                 <li key={joke.id}>
                   <Link to={joke.id}>{joke.name}</Link>
                 </li>
--- app/utils/sessions.server.ts
+++ app/utils/sessions.server.ts
@@ -40,23 +40,13 @@ const storage = createCookieSessionStorage({
   },
 });
 
-export async function createUserSession(userId: string, redirectTo: string) {
-  const session = await storage.getSession();
-  session.set('userId', userId);
-  return redirect(redirectTo, {
-    headers: {
-      'Set-Cookie': await storage.commitSession(session),
-    },
-  });
-}
-
 function getUserSession(request: Request) {
   return storage.getSession(request.headers.get('Cookie'));
 }
 
 export async function getUserId(request: Request) {
   const session = await getUserSession(request);
-  const userId = session.get('UserId');
+  const userId = session.get('userId');
   if (!userId || typeof userId !== 'string') return null;
   return userId;
 }
@@ -66,10 +56,46 @@ export async function requireUserId(
   redirectTo: string = new URL(request.url).pathname
 ) {
   const session = await getUserSession(request);
-  const userId = session.get('UserId');
+  const userId = session.get('userId');
   if (!userId || typeof userId !== 'string') {
     const searchParams = new URLSearchParams([['redirectTo', redirectTo]]);
     throw redirect(`/login?${searchParams}`);
   }
   return userId;
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (typeof userId !== 'string') {
+    return null;
+  }
+
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+      select: { id: true, username: true },
+    });
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
+
+export async function logout(request: Request) {
+  const session = await getUserSession(request);
+  return redirect('/login', {
+    headers: {
+      'Set-Cookie': await storage.destroySession(session),
+    },
+  });
+}
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set('userId', userId);
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session),
+    },
+  });
 }
